a1c2291c41a09483916c54e44912dd8f52289dc1
quentin.epron.etu <quentin.epron.etu@univ-lille.fr>
2023-10-05 00:27:08 +0200

    Fixed brush dynamism and refactored brush script

--- Scripts/Brush.gd
+++ Scripts/Brush.gd
@@ -13,50 +13,34 @@ var mouse_pos : Vector2
 
 func _draw():
 	var radius = $CollisionShape2D.shape.radius
-	draw_arc(Vector2.ZERO, radius, 0, TAU, 20, Color.BLUE, -1., true)
+	draw_arc(Vector2.ZERO, radius, 0, TAU, 20, Color.BLUE, .5, true)
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		mouse_pos = event.position
+		mouse_pos = event.global_position
 		move_brush()
+		set_nearest_target_port()
 
 func move_brush():
 	position = mouse_pos
 
-func _on_area_entered(area):
-	if not area is Port:
-		return
-	
-	var port = area
-	
-	# swap with new port if closer to cursor than target port else assign directly
-	if target_port != null:
-		swap_target_port(port)
-	else:
-		target_port = port
-
-func _on_area_exited(area):
-	if not area is Port:
-		return
-	
-	var port = area
-	
-	# unselect target port and set to null if its the one exiting the brush radius
-	if target_port == port:
-		target_port.targeted = false
-		target_port = null
-	
-	# set nearest port as target if there's one
-	var nearest_ports = get_overlapping_areas().filter(func(area): return area is Port)
-	if nearest_ports.size() > 0:
-		target_port = nearest_ports[0]
-		for i in range(1, nearest_ports.size()):
-			swap_target_port(nearest_ports[i])
+# set the nearest port in range as the new target
+func set_nearest_target_port():
+	var ports = get_overlapping_areas().filter(func(area): return area is Port)
+	replace_target_port()
+	if ports.size() > 0:
+		target_port = ports[0]
+		for i in range(1, ports.size()):
+			swap_target_port(ports[i])
 
 # swap target port with new one based on distance
-func swap_target_port(new):
-	var dist_new = new.position.distance_squared_to(mouse_pos)
-	var dist_target_port = target_port.position.distance_squared_to(mouse_pos)
+func swap_target_port(new_port):
+	var dist_new = new_port.global_position.distance_squared_to(mouse_pos)
+	var dist_target_port = target_port.global_position.distance_squared_to(mouse_pos)
 	if dist_new < dist_target_port:
+		replace_target_port(new_port)
+
+func replace_target_port(new_port: Port = null):
+	if target_port != null:
 		target_port.targeted = false
-		target_port = new
+	target_port = new_port
